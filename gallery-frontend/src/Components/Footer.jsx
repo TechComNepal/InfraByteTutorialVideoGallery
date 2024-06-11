b26385b23754c990aed0c16bd812e7824c76aacd
psynon1 <168067150+psynon1@users.noreply.github.com>
2024-06-11 16:28:59 +0545

    done

--- gallery-frontend/src/Components/Footer.jsx
+++ gallery-frontend/src/Components/Footer.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+    <p>
+      <img src="mywebsite.jpg" alt="bar" width="100" hight="100"/>
+    </p>
+    <nav>
+        <ul>
+            <li><a href="#privacy">Privacy Policy</a></li>
+            <li><a href="#terms">Terms of Service</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+        </ul>
+    </nav>
+</footer>
   )
 }
 
--- gallery-frontend/src/Components/Header.jsx
+++ gallery-frontend/src/Components/Header.jsx
@@ -4,11 +4,16 @@ import {Navbar, Nav, Form, Button} from 'react-bootstrap'
 function Header() {
   return (
     <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+
+        <Navbar.Brand href="#home" >
+        <img src="nonon.jpg" alt="logo" width="100" hight="100" />
+        </Navbar.Brand>
         <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/signin">Sign In</Nav.Link>
+            <Nav.Link href ="/login">login</Nav.Link>
         </Nav>
         <Form inline>
             <Form.Control type="text" placeholder="Search" className="me-sm-2" />
