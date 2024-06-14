0d552c118113cf61cd759726f983212516220d9d
psynon1 <168067150+psynon1@users.noreply.github.com>
2024-06-14 14:53:48 +0545

    .

--- gallery-frontend/src/Pages/Homepage.jsx
+++ gallery-frontend/src/Pages/Homepage.jsx
@@ -4,6 +4,7 @@ import '../Assets/Css/Homepage.css'
 function Homepage() {
   return (
 <div className="home-page">
+  
             <h1>Enroll Video Course</h1>
             <li><a href="https://infrabyte.com.au/tutorial/courses/">
             <img src="learnmore.png" alt="logo" width="300" hight="300" />
