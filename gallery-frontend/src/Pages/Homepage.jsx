9604f2e27c7caacea125a90f61716090e5dd7cf8
Ragen Maharjan0 <ragenmah99@gmail.com>
2024-06-26 12:16:17 +0545

    made login page responsive

--- gallery-frontend/src/Pages/Homepage.jsx
+++ gallery-frontend/src/Pages/Homepage.jsx
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Assets/Css/Homepage.css";
 import { useNavigate } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
 import LoginPage from "./LoginPage";
 
 function Homepage() {
-  let navigate = useNavigate();
-  return (  
-    <div className="d-flex">
-    <div className="home-page ">
-      <div className="overlay">
-        <div className="inner-container">
-        <div>  
-        <h6 className="heading1">Enroll InfraByte Videos</h6>
-          <h1 className="heading2 mt-2 mb-4">InfraByte videos are ready to play</h1>
-          
-          <a
-            className="button-container "
-           href="https://infrabyte.com.au/"
-          >
-            Visit site
-          </a>
+
+  return (
+    <div className="home-container">
+      <div className="home-page hide-container">
+        <div className="overlay">
+          <div className="inner-container">
+            <div>
+              <h6 className="heading1">Enroll InfraByte Videos</h6>
+              <h1 className="heading2 mt-2 mb-4">
+                InfraByte videos are ready to play
+              </h1>
+
+              <a
+                className="button-container "
+                href="https://infrabyte.com.au/"
+                target="_blank"
+              >
+                Visit site
+              </a>
+            </div>
+          </div>
         </div>
-        
-        </div>
-     
       </div>
-    </div>
-            <LoginPage/>
+
+      <LoginPage />
     </div>
   );
 }
